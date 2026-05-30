98e51ac3fc5ff9203dd6e112f6c8cccb1fa6885f
Phan Viet <phanquocviet1990@icloud.com>
2026-05-30 15:24:07 +0700

    first website

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#0f172a",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "40px",
+      fontWeight: "bold"
+    }}>
+      Phan Viet
+    </div>
+  )
+}
